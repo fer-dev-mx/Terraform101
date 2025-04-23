d85c442835804be2b05bb1390935157761d09402
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-04-23 02:06:53 -0600

    Add output definitions for application and environment names

---
+++ LAB1/outputs.tf
@@ -0,0 +1,9 @@
+output "application_name" {
+  value = var.application_name
+}
+output "environment_name" {
+  value = var.environment_name
+}
+output "environment_preffix" {
+  value = local.environment_name
+}
