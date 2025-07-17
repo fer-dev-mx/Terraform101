cfa5f53b4d55f4a16133e9964497aa1fdc1392a9
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-07-17 15:39:38 -0600

    Refactor: variable definitions for consistency and clarity

--- LAB1/variables.tf
+++ LAB1/variables.tf
@@ -1,9 +1,13 @@
 variable "application_name" {
-
+  type = string
 }
 variable "environment_name" {
-
+  type = string
 }
 variable "api_key" {
+  type      = string
   sensitive = true
+}
+variable "instance_count" {
+  type = number
 }
