4f9200850ae329eb77b67d1be40dbde6df9f3aee
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-10-07 09:09:29 -0600

    Parameterize resource group name and location
    Updated the resource group definition in main.tf to use variables for name and location. Added application_name, environment_name, and primary_location variables in variables.tf to support flexible configuration.

--- LAB2/main.tf
+++ LAB2/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "main" {
-  name     = "rg-terraform-lab2"
-  location = "Mexico Central"
+  name     = "rg-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
 }
