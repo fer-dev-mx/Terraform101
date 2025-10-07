d9b3163a021092483e8b87fe92a823ea1bb86802
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-10-07 09:35:01 -0600

    Update storage account name and replication type
    Changed the storage account name prefix to 'stlab' and updated the replication type from 'GRS' to 'LRS' for the azurerm_storage_account resource.

--- LAB2/main.tf
+++ LAB2/main.tf
@@ -10,9 +10,9 @@ resource "random_string" "suffix" {
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = "st${random_string.suffix.result}"
+  name                     = "stlab${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 }
