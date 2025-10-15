33a0360585f29f4f962c28f000f81cf27b2d3a2e
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-10-15 10:48:57 -0600

    Add provider configuration to versions.tf
    Introduced required_providers block specifying azurerm and random providers with their respective versions. Added azurerm provider configuration to enable Azure resource management.

--- LAB3/versions.tf
+++ LAB3/versions.tf
@@ -1,1 +1,21 @@
 
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.47.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
