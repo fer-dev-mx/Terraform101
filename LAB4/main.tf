d55e0b139e84873c1f2eb70761e9c5d4129e7ab6
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-10-23 12:52:31 -0600

    Set Key Vault tenant_id from client config
    Replaces the hardcoded tenant_id in the azurerm_key_vault resource with the value from the current Azure client configuration using the azurerm_client_config data source. This improves flexibility and reduces manual configuration.

--- LAB4/main.tf
+++ LAB4/main.tf
@@ -9,11 +9,14 @@ resource "random_string" "keyvault_suffix" {
   special = false
 }
 
+data "azurerm_client_config" "current" {
+}
+
 resource "azurerm_key_vault" "main" {
   name                = "kv-${var.application_name}-${var.environment_name}-${random_string.keyvault_suffix.result}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  tenant_id           = ""
+  tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
 }
