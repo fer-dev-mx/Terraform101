439632ddf8bf2f17974cf075757040a439dc3594
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-10-28 09:38:36 -0600

    Add subnets and tags to Azure resources
    Added four subnets (alpha, bravo, charlie, delta) to the virtual network and updated its address space to 10.1.0.0/22. Also added a 'course' tag to the resource group for identification.

--- LAB5/main.tf
+++ LAB5/main.tf
@@ -1,11 +1,46 @@
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.application_name}-${var.environment_name}"
   location = var.primary_location
+  tags = {
+    course = "yes"
+  }
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "vnet-${var.application_name}-${var.environment_name}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.1.0.0/24"]
+  address_space       = ["10.1.0.0/22"]
+}
+
+// 10.1.0.0/24
+resource "azurerm_subnet" "alpha" {
+  name                 = "snet-alpha"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.0.0/24"]
+}
+
+// 10.1.1.0/24
+resource "azurerm_subnet" "bravo" {
+  name                 = "snet-bravo"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+// 10.1.2.0/24
+resource "azurerm_subnet" "charlie" {
+  name                 = "snet-charlie"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
+
+// 10.1.3.0/24
+resource "azurerm_subnet" "delta" {
+  name                 = "snet-delta"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.3.0/24"]
 }
