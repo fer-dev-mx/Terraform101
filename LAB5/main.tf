34d611f5b0347e56c2eed2669f7cc33e4dab2136
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-10-28 00:33:36 -0600

    Add Azure virtual network resource to main.tf
    Introduces an azurerm_virtual_network resource to provision a virtual network in the same resource group and location as defined by the application and environment variables.

--- LAB5/main.tf
+++ LAB5/main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-${var.application_name}-${var.environment_name}"
   location = var.primary_location
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.1.0.0/24"]
+}
