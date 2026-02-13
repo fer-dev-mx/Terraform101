98d9c4572cc027121047bae11c6324e5caa48264
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2026-02-12 18:24:59 -0600

    feature: Add Azure Bastion host and public IP
    Introduce azurerm_public_ip and azurerm_bastion_host resources to provide Bastion connectivity for the environment. The public IP uses Static allocation and Standard SKU and is named using application and environment variables; the bastion host is configured with an IP configuration that references the bastion subnet and the new public IP. This enables secure browser-based RDP/SSH access to VMs in the virtual network.

--- LAB5/main.tf
+++ LAB5/main.tf
@@ -75,3 +75,23 @@ resource "azurerm_network_security_group" "remote_access" {
 data "http" "my_ip" {
   url = "https://ipv4.icanhazip.com"
 }
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "pip-${var.application_name}-${var.environment_name}-bastion"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "main" {
+  name                = "bas-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
