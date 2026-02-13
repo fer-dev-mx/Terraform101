405765ebccb047883492da799ec88cce947abecd
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2026-02-13 13:21:26 -0600

    refactor: Remove local private key file resource
    Delete the local_file resource that wrote tls_private_key.vm1.private_key_pem to ~/.ssh/vm1. This prevents persisting the private key to disk (improving security); the public key file resource (~/.ssh/vm1.pub) remains unchanged.

--- LAB6/main.tf
+++ LAB6/main.tf
@@ -38,12 +38,6 @@ resource "tls_private_key" "vm1" {
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
-  content         = tls_private_key.vm1.private_key_pem
-  filename        = pathexpand("~/.ssh/vm1")
-  file_permission = "0600"
-}
-
 resource "local_file" "public_key" {
   content  = tls_private_key.vm1.public_key_openssh
   filename = pathexpand("~/.ssh/vm1.pub")
